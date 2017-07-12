db407c67abc58fe30d2b76933fc6ce18072b9523
Adam Lovastyik <adam@lovastyik.hu>
2017-07-12 21:49:54 +0200

    Initial commit

---
+++ FrontPageMe/UIImage.swift
@@ -0,0 +1,101 @@
+//
+//  UIImage.swift
+//  FrontPageMe
+//
+//  Created by Adam Lovastyik [Standard] on 12/07/2017.
+//  Copyright © 2017 Decos B.V. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreGraphics
+
+extension UIImage {
+    
+    func masked(with maskImage: UIImage) -> UIImage {
+        
+        let maskRef = maskImage.cgImage
+        
+        let mask = CGImage(
+            maskWidth: maskRef!.width,
+            height: maskRef!.height,
+            bitsPerComponent: maskRef!.bitsPerComponent,
+            bitsPerPixel: maskRef!.bitsPerPixel,
+            bytesPerRow: maskRef!.bytesPerRow,
+            provider: maskRef!.dataProvider!,
+            decode: nil,
+            shouldInterpolate: false)
+        
+        let masked = self.cgImage!.masking(mask!)
+        let maskedImage = UIImage(cgImage: masked!)
+        
+        // No need to release. Core Foundation objects are automatically memory managed.
+        
+        return maskedImage
+    }
+    
+    func fixOrientation() -> UIImage {
+        
+        // No-op if the orientation is already correct
+        if ( self.imageOrientation == UIImageOrientation.up ) {
+            return self;
+        }
+        
+        // We need to calculate the proper transformation to make the image upright.
+        // We do it in 2 steps: Rotate if Left/Right/Down, and then flip if Mirrored.
+        var transform: CGAffineTransform = CGAffineTransform.identity
+        
+        if ( self.imageOrientation == UIImageOrientation.down || self.imageOrientation == UIImageOrientation.downMirrored ) {
+            transform = transform.translatedBy(x: self.size.width, y: self.size.height)
+            transform = transform.rotated(by: CGFloat(Double.pi))
+        }
+        
+        if ( self.imageOrientation == UIImageOrientation.left || self.imageOrientation == UIImageOrientation.leftMirrored ) {
+            transform = transform.translatedBy(x: self.size.width, y: 0)
+            transform = transform.rotated(by: CGFloat(Double.pi / 2.0))
+        }
+        
+        if ( self.imageOrientation == UIImageOrientation.right || self.imageOrientation == UIImageOrientation.rightMirrored ) {
+            transform = transform.translatedBy(x: 0, y: self.size.height);
+            transform = transform.rotated(by: CGFloat(-Double.pi / 2.0));
+        }
+        
+        if ( self.imageOrientation == UIImageOrientation.upMirrored || self.imageOrientation == UIImageOrientation.downMirrored ) {
+            transform = transform.translatedBy(x: self.size.width, y: 0)
+            transform = transform.scaledBy(x: -1, y: 1)
+        }
+        
+        if ( self.imageOrientation == UIImageOrientation.leftMirrored || self.imageOrientation == UIImageOrientation.rightMirrored ) {
+            transform = transform.translatedBy(x: self.size.height, y: 0);
+            transform = transform.scaledBy(x: -1, y: 1);
+        }
+        
+        // Now we draw the underlying CGImage into a new context, applying the transform
+        // calculated above.
+        let ctx: CGContext = CGContext(data: nil, width: Int(self.size.width), height: Int(self.size.height),
+                                       bitsPerComponent: self.cgImage!.bitsPerComponent, bytesPerRow: 0,
+                                       space: self.cgImage!.colorSpace!,
+                                       bitmapInfo: self.cgImage!.bitmapInfo.rawValue)!;
+        
+        ctx.concatenate(transform)
+        
+        if ( self.imageOrientation == UIImageOrientation.left ||
+            self.imageOrientation == UIImageOrientation.leftMirrored ||
+            self.imageOrientation == UIImageOrientation.right ||
+            self.imageOrientation == UIImageOrientation.rightMirrored ) {
+            ctx.draw(self.cgImage!, in: CGRect(x: 0,y: 0,width: self.size.height,height: self.size.width))
+        } else {
+            ctx.draw(self.cgImage!, in: CGRect(x: 0,y: 0,width: self.size.width,height: self.size.height))
+        }
+        
+        // And now we just create a new UIImage from the drawing context and return it
+        return UIImage(cgImage: ctx.makeImage()!)
+    }
+    
+    func cropped(to rect: CGRect) -> UIImage {
+        
+        let imageRef:CGImage = self.cgImage!.cropping(to: rect)!
+        let cropped:UIImage = UIImage(cgImage:imageRef)
+        return cropped
+    }
+}
